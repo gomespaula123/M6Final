c086c5d16d05f43ba5b38685d6d8922fb6f3e15a
Paula <gomespaula123@gmail.com>
2020-01-20 17:15:47 +0100

    openCV following tutorial (of face detection, but without the face detection bit) - the webcam and image display work

--- M6opencv.py
+++ M6opencv.py
@@ -1,3 +1,30 @@
-print("openCVfileTest")
+import cv2
 
-print("I added a thing from Pycharm")
+# reference the tutorial !!!!!!!!!!!!!!!!! https://towardsdatascience.com/https-medium-com-dilan-jay-face-detection-model-on-webcam-using-python-72b382699ee9
+
+video_capture = cv2.VideoCapture(0)
+
+img_counter = 0
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = video_capture.read()
+
+    k = cv2.waitKey(1)
+
+    # Display the resulting frame
+    # cv2.imshow(window_name, image)
+    cv2.imshow('Windowname', frame)
+
+    if k % 256 == 27:  # ESC Pressed
+        break
+    elif k % 256 == 32:
+        # SPACE pressed
+        img_name = "facedetect_webcam_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+# When everything is done, release the capture
+video_capture.release()
+cv2.destroyAllWindows()
