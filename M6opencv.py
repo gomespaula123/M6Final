27c0f4a032c42d04f961fbf0ef17140e5312330f
gomespaula123 <p.clemensvilla@student.utwente.nl>
2020-01-22 23:36:26 +0100

    23 35 test on other computer

--- M6opencv.py
+++ M6opencv.py
@@ -1,30 +1,38 @@
 import cv2
-# this changed
-# reference the tutorial !!!!!!!!!!!!!!!!! https://towardsdatascience.com/https-medium-com-dilan-jay-face-detection-model-on-webcam-using-python-72b382699ee9
+import numpy as np
+
+# Set up the detector with default parameters.
 
 video_capture = cv2.VideoCapture(0)
 
-img_counter = 0
-
 while True:
+    detector = cv2.SimpleBlobDetector()
+    lower_range = np.array([120, 100, 240])
+    upper_range = np.array([190, 160, 255])
     # Capture frame-by-frame
     ret, frame = video_capture.read()
+    bgr = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    k = cv2.waitKey(1)
+    paperThing = cv2.inRange(bgr, lower_range, upper_range)
 
+    # Detect blobs.
+    keypoints = detector.detect(paperThing)
+
+    cv2.waitKey(2)
     # Display the resulting frame
-    # cv2.imshow(window_name, image)
-    cv2.imshow('Windowname', frame)
+    cv2.imshow('Paper thing', paperThing)
+    cv2.imshow('THE EYE', frame)
 
-    if k % 256 == 27:  # ESC Pressed
-        break
-    elif k % 256 == 32:
+    '''   if k%256 == 27: #ESC Pressed
+        break'''
+''' elif k == orb('g'):
         # SPACE pressed
         img_name = "facedetect_webcam_{}.png".format(img_counter)
         cv2.imwrite(img_name, frame)
         print("{} written!".format(img_name))
         img_counter += 1
 
+
 # When everything is done, release the capture
 video_capture.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows()'''
