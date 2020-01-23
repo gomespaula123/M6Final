7d7556e6d0702bdf0f9005d5176c6b5ae5914e86
gomespaula123 <p.clemensvilla@student.utwente.nl>
2020-01-23 04:04:03 +0100

    4 03 getting color to be read out

--- M6opencv.py
+++ M6opencv.py
@@ -1,38 +1,36 @@
 import cv2
 import numpy as np
 
-# Set up the detector with default parameters.
 
+# Turn on the camera
 video_capture = cv2.VideoCapture(0)
 
 while True:
-    detector = cv2.SimpleBlobDetector()
+    # Determine the spotting range of RED
     lower_range = np.array([120, 100, 240])
     upper_range = np.array([190, 160, 255])
+
     # Capture frame-by-frame
     ret, frame = video_capture.read()
     bgr = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
+    # Filter out from the capture the RED object
     paperThing = cv2.inRange(bgr, lower_range, upper_range)
 
-    # Detect blobs.
-    keypoints = detector.detect(paperThing)
-
-    cv2.waitKey(2)
-    # Display the resulting frame
+    # Determine if there is indeed an object in the image
+    hight = int(format(paperThing.shape[0]))
+    width = int(format(paperThing.shape[1]))
+    whiteness = 0
+    for i in range(hight - 1):
+        for j in range(width - 1):
+            if paperThing[i, j] > 200:
+                whiteness = whiteness + 1
+    if whiteness >= hight * width / 10:
+        # Object confirmed
+        print('RED!!!')
+    # Display the resulting captures
     cv2.imshow('Paper thing', paperThing)
     cv2.imshow('THE EYE', frame)
 
-    '''   if k%256 == 27: #ESC Pressed
-        break'''
-''' elif k == orb('g'):
-        # SPACE pressed
-        img_name = "facedetect_webcam_{}.png".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
-
-
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()'''
+    # Waits for next frame
+    cv2.waitKey(200)
